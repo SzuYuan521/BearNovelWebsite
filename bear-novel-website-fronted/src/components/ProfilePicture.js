d99471251f7b994aba7736facde89744b4d64b06
Bear <SzuYuan521@gmail.com>
2024-09-08 01:32:31 +0800

    [!] 改使用 size 參數控制頭像大小

--- bear-novel-website-fronted/src/components/ProfilePicture.js
+++ bear-novel-website-fronted/src/components/ProfilePicture.js
@@ -1,13 +1,28 @@
 import React from "react";
 import "../css/auth.css";
 
-const ProfilePicture = ({ profilePicture }) => {
+const ProfilePicture = ({ profilePicture, size }) => {
+  const pictureStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+  };
+
   return (
     <div className="profile-picture-container">
       {profilePicture ? (
-        <img src={profilePicture} alt="Profile" className="profile-picture" />
+        <img
+          src={profilePicture}
+          alt="Profile"
+          className="profile-picture"
+          style={pictureStyle}
+        />
       ) : (
-        <img src="/img/logo425.png" alt="Profile" className="profile-picture" />
+        <img
+          src="/img/logo425.png"
+          alt="Profile"
+          className="profile-picture"
+          style={pictureStyle}
+        />
       )}
     </div>
   );
