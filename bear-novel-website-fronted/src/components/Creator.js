c46eb983b7257f62ecdf37e7a66bb2809a13b180
Bear <SzuYuan521@gmail.com>
2024-09-09 03:46:59 +0800

    [+] 新增 Creator 創作者中心
    1. 新增Creator.js
    2. 創作者中心顯示user的照片跟暱稱還有稱號(暫定為所有人都是創作作家)
    3. 有四個按鈕(暫時只有一個功能:我的作品管理)
    4. 新增pen-icon

---
+++ bear-novel-website-fronted/src/components/Creator.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import ProfilePicture from "./ProfilePicture";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../redux/slices/userSlice";
+import { useModal } from "../contexts/ModalContext";
+import "../css/creator.css";
+
+const Creator = () => {
+  const dispatch = useDispatch();
+  const { isLoggedIn, user, userLoaded } = useSelector((state) => state.user);
+  const { openModal } = useModal();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (userLoaded && !isLoggedIn) {
+      openModal("請先登入", "登入會員後，享受更多會員福利!", () => {
+        navigate("/login");
+      });
+    }
+  }, [userLoaded, isLoggedIn, openModal, navigate]);
+
+  return (
+    <div className="creator-container ">
+      {isLoggedIn && (
+        <div className="creator-user-info">
+          <ProfilePicture profilePicture={user.profilePicture} size={100} />
+          <div className="mt-3">
+            <p className="creator-user-name">{user.userName}</p>
+            <p className="creator-user-identity">
+              <i className={"bi-star-fill"}> </i>
+              創作作家
+            </p>
+          </div>
+        </div>
+      )}
+      {isLoggedIn && (
+        <div className="creator-management-panel">
+          <Link
+            to="/novel/create"
+            className="creator-management-button my-novels-button"
+          >
+            <img
+              src="/img/ui/pen-icon-2.png"
+              alt="我的作品"
+              className="my-novels-icon"
+            />
+          </Link>
+          <button className="creator-management-button comments-button"></button>
+          <button className="creator-management-button data-center-button"></button>
+          <button className="creator-management-button income-button"></button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Creator;
