4b6e25d0e895b6a5935332fb555a607a2d26d266
Bear <SzuYuan521@gmail.com>
2024-09-13 19:19:23 +0800

    [!] 新增 check-author 路徑

--- bear-novel-website-fronted/src/api/api.js
+++ bear-novel-website-fronted/src/api/api.js
@@ -13,19 +13,20 @@ const authRequiredPaths = [
   "/novels/", // 創建、更新、刪除小說
   "/novels/my-novels",
   "/novels//like", // 點讚或取消點讚
+  "novels//check-author", // 檢查是不是該小說作者
 ];
 
-/// 可選認證的 API 列表
+/// 可選認證的 API 列表(即使登出也會繼續呼叫 API)
 const optionalAuthPaths = ["/novels"];
 
 /// 檢查是否需要認證
 const isAuthRequired = (url) => {
   return authRequiredPaths.some((path) => {
     if (path.endsWith("/")) {
-      // 對於以 '/' 結尾的路徑，我們檢查 URL 是否以該路徑開始
+      // 對於以 '/' 結尾的路徑, 檢查 URL 是否以該路徑開始
       return url.startsWith(path);
     } else {
-      // 對於其他路徑，我們使用完全匹配
+      // 對於其他路徑, 使用完全匹配
       return url === path || url.startsWith(path + "/");
     }
   });
@@ -39,13 +40,13 @@ const isOptionalAuth = (url) => {
 // 請求攔截器
 axiosInstance.interceptors.request.use(
   async (config) => {
-    // 如果是需要認證的 API 或可選認證的 API，嘗試刷新 token
+    // 如果是需要認證的 API 或可選認證的 API, 嘗試刷新 token
     if (isAuthRequired(config.url) || isOptionalAuth(config.url)) {
       try {
         await axiosInstance.post("/users/refresh-token", {}, { timeout: 5000 });
       } catch (error) {
         console.warn("Token 刷新失敗");
-        // 對於可選認證的 API，即使刷新失敗也繼續請求
+        // 對於可選認證的 API, 即使刷新失敗也繼續請求
         if (isOptionalAuth(config.url)) {
           console.log("繼續發送請求，可能以未登錄狀態訪問");
         } else if (isAuthRequired(config.url)) {
@@ -68,20 +69,20 @@ axiosInstance.interceptors.response.use(
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
-      // 對於可選認證的 API，即使收到 401 也繼續請求
+      // 對於可選認證的 API, 即使收到 401 也會繼續請求
       if (isOptionalAuth(originalRequest.url)) {
         console.log("以未登錄狀態重試請求");
         return axiosInstance(originalRequest);
       }
 
-      // 對於必須認證的 API，嘗試刷新 token
+      // 對於必須認證的 API, 嘗試刷新 token
       if (isAuthRequired(originalRequest.url)) {
         try {
           await axiosInstance.post("/users/refresh-token");
           return axiosInstance(originalRequest);
         } catch (refreshError) {
           console.error("Token 刷新失敗，可能需要重新登錄");
-          // 這裡可以觸發重新登錄流程
+          // 這裡之後再添加觸發重新登錄流程
           return Promise.reject(refreshError);
         }
       }
