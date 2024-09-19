34c5902e662384a9f470edc606893014aff6c1ab
Bear <SzuYuan521@gmail.com>
2024-09-19 16:23:28 +0800

    [!] 新增 AWS Lambda配置(尚未測試)

---
+++ BearNovelWebsiteApi/Lambda/LambdaEntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+
+namespace BearNovelWebsiteApi.Lambda
+{
+    public class LambdaEntryPoint : APIGatewayHttpApiV2ProxyFunction
+    {
+        // 初始化 ASP.NET Core 應用程式的 WebHostBuilder
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Program>();
+        }
+    }
+}
